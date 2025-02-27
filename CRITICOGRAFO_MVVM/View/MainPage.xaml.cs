9dea62129c3c53528a565573e5ee60864b956a11
floresrf <arfr10@hotmail.com>
2025-02-27 12:53:36 -0700

    First Commit

---
+++ CRITICOGRAFO_MVVM/View/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using CRITICOGRAFO_MVVM.ViewModels;
+
+namespace CRITICOGRAFO_MVVM
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+    }
+
+}
